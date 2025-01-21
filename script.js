bf8eb7142a9f866d0ffda1c06bb2b08d73ed2b1c
quangvinhj <115966487+quangvinhj@users.noreply.github.com>
2025-01-21 11:59:57 +0700

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " Phòng khám chuyên khoa phụ sản KIM TÍNH. All rights reserved.</p>";
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='dichvu.html'>Dịch Vụ</a></li>" +
+"<li><a href='thuoc.html'>Thuốc</a></li>" +
+"<li><a href='taman.html'>Tâm An</a></li>" +
+"<li><a href='tuoithai-v21.html'>Tuổi thai</a></li>" +
+"</ul>";
